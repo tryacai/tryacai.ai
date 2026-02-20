f16d131996839b13a372d4320e3741f24609beb4
tryacai <team@tryacai.ai>
2026-02-20 04:50:23 +0000

    fix error

--- app/(marketing)/plumbing/page.tsx
+++ app/(marketing)/plumbing/page.tsx
@@ -180,7 +180,7 @@ export default function PlumbingPage() {
               Get Started
             </Button>
           </div>
-        </div>
+        </section>
       </Container>
     </div>
   );
